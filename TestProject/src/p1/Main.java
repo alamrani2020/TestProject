621569bc20968542c05e842ab812a186467c06e6
Majed <majid-x66@hotmail.com>
2020-03-01 10:13:55 +0400

    This commit on first program
    Signed-off-by: Majed <majid-x66@hotmail.com>

---
+++ TestProject/src/p1/Main.java
@@ -0,0 +1,28 @@
+package p1;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MuscatBank mb =  new MuscatBank();
+		mb.setEmId(10);
+		mb.setEmName("Majed");
+		mb.setEmPosition("Asistent Manager");
+		mb.setEmStatus("Active");
+		
+		
+		System.out.println(
+			"EmployeeID: " + mb.getEmId() +"\n"+
+			"Employee Name:" + mb.getEmName() + "\n"+
+			"Employee Position:" + mb.getEmPosition()+"\n"+
+			"Employee Status: " + mb.getEmStatus());
+		
+		
+		
+	}
+	
+	}
+
+
---
+++ TestProject/src/p1/MuscatBank.java
@@ -0,0 +1,37 @@
+package p1;
+
+public class MuscatBank {
+	
+	
+	private  int  emId;
+	private  String  emName;
+	private  String  emPosition;
+	private  String  emStatus;
+	public int getEmId() {
+		return emId;
+	}
+	public void setEmId(int emId) {
+		this.emId = emId;
+	}
+	public String getEmName() {
+		return emName;
+	}
+	public void setEmName(String emName) {
+		this.emName = emName;
+	}
+	public String getEmPosition() {
+		return emPosition;
+	}
+	public void setEmPosition(String emPosition) {
+		this.emPosition = emPosition;
+	}
+	public String getEmStatus() {
+		return emStatus;
+	}
+	public void setEmStatus(String emStatus) {
+		this.emStatus = emStatus;
+	}
+	
+	
+
+}
